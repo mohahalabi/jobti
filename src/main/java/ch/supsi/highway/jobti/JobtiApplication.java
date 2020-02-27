a00d419c86689e019bd298f9ccea0152af5d81d2
roberto.bignasca@student.supsi.ch <bignasca@gmail.com>
2020-02-27 16:34:53 +0100

    Io ci sono riuscito, merdegit status

---
+++ src/main/java/ch/supsi/highway/jobti/JobtiApplication.java
@@ -0,0 +1,13 @@
+package ch.supsi.highway.jobti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/supsi/highway/jobti/JobtiApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.supsi.highway.jobti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobtiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
