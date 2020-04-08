c0b014a1e801940e15d35363f05f84a3c9328ded
Petkov <david.petkov@student.supsi.ch>
2020-04-08 11:12:46 +0200

    merged

--- src/main/java/ch/supsi/highway/jobti/model/Education.java
+++ src/main/java/ch/supsi/highway/jobti/model/Education.java
@@ -19,7 +19,58 @@ public class Education {
     @NotNull
     private Date end;
 
+    private String schoolType;
     private String school;
     private String degree;
 
+    public Education() {
+    }
+
+    public Education(Date begin, Date end, String schoolType, String school, String degree) {
+        this.begin=begin;
+        this.end=end;
+        this.school=school;
+        this.schoolType=schoolType;
+        this.degree=degree;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getSchoolType() {
+        return schoolType;
+    }
+
+    public void setSchoolType(String schoolType) {
+        this.schoolType = schoolType;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
 }
