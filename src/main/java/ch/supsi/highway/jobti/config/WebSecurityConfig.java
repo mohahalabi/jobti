27ba9815c088cc4e4a0309ec59a8ce963ad6b058
Petkov <david.petkov@student.supsi.ch>
2020-04-23 17:05:00 +0200

    fixed login error

--- src/main/java/ch/supsi/highway/jobti/config/WebSecurityConfig.java
+++ src/main/java/ch/supsi/highway/jobti/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .failureUrl("/login?error")
+                .failureUrl("/login?error=true")
                 .defaultSuccessUrl("/profilehome", true)
                 .and()
                 .logout()
